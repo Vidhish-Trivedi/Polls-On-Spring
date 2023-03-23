6f0d31162da818f9182eb50a89f5feac2c89d962
Vidhish-Trivedi <Vidhish.Trivedi@iiitb.ac.in>
2023-03-24 03:12:39 +0530

    Responses for Login/Signup, JWT Authentication, and Exception definitions

---
+++ polls/src/main/java/com/mypolls/polls/payload/ApiResponse.java
@@ -0,0 +1,31 @@
+package com.mypolls.polls.payload;
+
+public class ApiResponse {
+    private boolean success;
+    private String message;
+
+    public ApiResponse(boolean success, String message) {
+        this.success = success;
+        this.message = message;
+    }
+    
+    public boolean isSuccess() {
+        return this.success;
+    }
+
+    public boolean getSuccess() {
+        return this.success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }    
+}
