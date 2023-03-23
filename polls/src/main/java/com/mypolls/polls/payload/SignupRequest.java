9d84b866b33cb8caf27eb11dbdd6cc07beaba7ce
Vidhish-Trivedi <Vidhish.Trivedi@iiitb.ac.in>
2023-03-23 23:07:46 +0530

    Defined request/response payloads for Login/Signup API

---
+++ polls/src/main/java/com/mypolls/polls/payload/SignupRequest.java
@@ -0,0 +1,55 @@
+package com.mypolls.polls.payload;
+
+import javax.validation.constraints.*;;
+
+public class SignupRequest {
+    @NotBlank
+    @Size(min = 4, max = 40)
+    private String name;
+
+    @NotBlank
+    @Size(min = 3, max = 15)
+    private String username;
+
+    @NotBlank
+    @Size(max = 40)
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 8, max = 20)
+    private String password;
+
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
